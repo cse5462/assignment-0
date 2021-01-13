e8dd9fc3a968f50bb93a32b8c47a1d12f6642c51
Conner Graham <59581008+cgraham1092@users.noreply.github.com>
2021-01-12 19:08:15 -0500

    Added proper documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+This is the README file for Lab_0
+
+Name: Conner Graham
+
+This program can be run by entereing the command "hello"
+on the command line in the terminal and hitting enter. The
+expected output is "Hello world, I am Conner Graham".
--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+/**
+ * @brief Classic Hello-World program modified to also print my name.
+ * 
+ * @return If the return statement is used, the return value is used as the argument to the
+ * implicit call to exit(). The values zero and EXIT_SUCCESS indicate successful termination,
+ * the value EXIT_FAILURE indicates unsuccessful termination.
+ */
 int main() {
-  printf("Hello world, I am Conner Graham\n");
-  return 0;
+    printf("Hello world, I am Conner Graham\n");
+    return 0;
 }
